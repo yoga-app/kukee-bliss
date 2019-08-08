a9d1abe92aabe84d2192f5bb9dd55ebbf2267fa6
arstrel <arstrel@gmail.com>
2019-08-08 12:59:56 -0400

    hamburger nav in progress

--- client/src/components/nav/Nav.js
+++ client/src/components/nav/Nav.js
@@ -1,32 +1,69 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import './nav.css';
+import Breakpoint from 'react-socks';
+import CheeseburgerMenu from 'cheeseburger-menu'
+import HamburgerMenu from 'react-hamburger-menu'
+import MenuContent from '../menu-content/MenuContent'
 
 
+class Nav extends React.Component{
+  constructor(props) {
+    super(props)
 
-function Nav(props){
+    this.state = {
+      menuOpen: false,
+    }
+  }
 
-  const doLogoutNowPlease = () =>{
-      props.pleaseLogOut()
+  openMenu() {
+    this.setState({ menuOpen: true })
+  }
+
+  closeMenu() {
+    this.setState({ menuOpen: false })
+  }
+
+  doLogoutNowPlease = () =>{
+      this.props.pleaseLogOut()
       .then(()=>{
-          props.getUser();
+          this.props.getUser();
       })
   }
 
+  render () {
   return(
+    <div>
+    <Breakpoint small down>
+    <div>
+      <CheeseburgerMenu
+        isOpen={this.state.menuOpen}
+        closeCallback={this.closeMenu.bind(this)}>
+        <MenuContent closeCallback={this.closeMenu.bind(this)}/>
+      </CheeseburgerMenu>
+      
+      <HamburgerMenu
+        isOpen={this.state.menuOpen}
+        menuClicked={this.openMenu.bind(this)}
+        width={32}
+        height={24}
+        strokeWidth={3}
+        rotate={0}
+        color='black'
+        borderRadius={0}
+        animationDuration={0.5}
+      />
+      
+      <div >
+        <h1>Cheeseburger Menu demo</h1>
+        <p>Click the hamburger icon to open menu</p>
+      </div>
+    </div>
+    </Breakpoint>
 
-    // nav-links:
-    // home
-    // about
-    // prices
-    // signup
-    // FAQ
-    // gallery
-    // schedules
-    // newsletter feed
-
-    <nav>
-      <NavLink className="logo-link" to="/" onClick={props.hideLoginAndSignupForms}>
+    <Breakpoint medium up>
+        <nav>
+      <NavLink className="logo-link" to="/" onClick={this.props.hideLoginAndSignupForms}>
         <div className="logo">
           <img className="kukee-img" src="/images/logo.svg" alt="logo" />
           <p className="kukee">Kukee <span>Bliss Yoga</span></p>
@@ -36,53 +73,56 @@ function Nav(props){
         <NavLink exact to="/" 
         activeClassName="nav-link-active" 
         className="nav-link"
-        onClick={props.hideLoginAndSignupForms}> home</NavLink>
+        onClick={this.props.hideLoginAndSignupForms}> home</NavLink>
 
-        {!props.theUser && 
+        {!this.props.theUser && 
           <span>
-            <button className="nav-button" onClick = {()=> props.toggleForm('login')}>login</button>
-            <button className="nav-button" onClick = {()=> props.toggleForm('signup')}>sign up</button>
+            <button className="nav-button" onClick = {()=> this.props.toggleForm('login')}>login</button>
+            <button className="nav-button" onClick = {()=> this.props.toggleForm('signup')}>sign up</button>
           </span>
         }
 
         <NavLink exact to="/classes" activeClassName="nav-link-active" 
         className="nav-link"
-        onClick={props.hideLoginAndSignupForms}
+        onClick={this.props.hideLoginAndSignupForms}
         >classes</NavLink>
 
         <NavLink exact to="/aboutus" activeClassName="nav-link-active" 
         className="nav-link"
-        onClick={props.hideLoginAndSignupForms}
+        onClick={this.props.hideLoginAndSignupForms}
         >about us</NavLink>
 
         <NavLink exact to="/gallery" activeClassName="nav-link-active" 
         className="nav-link"
-        onClick={props.hideLoginAndSignupForms}
+        onClick={this.props.hideLoginAndSignupForms}
         >gallery</NavLink>
 
         {/* <NavLink to="/newsletter" className="nav-link">newsletter</NavLink> */}
 
-        {props.theUser && 
+        {this.props.theUser && 
           <span>
-            <button className="nav-button" onClick = {doLogoutNowPlease}>logout</button>
+            <button className="nav-button" onClick = {this.doLogoutNowPlease}>logout</button>
           </span>
         }
 
-        {props.theUser && 
+        {this.props.theUser && 
           <NavLink to="/profile" activeClassName="nav-link-active" 
           className="nav-link profile"
           >
-            <img className="profile-pic" alt="profile" src={props.theUser.picture} />
+            <img className="profile-pic" alt="profile" src={this.props.theUser.picture} />
             <div>
-              {props.theUser.firstName} {props.theUser.lastName}
+              {this.props.theUser.firstName} {this.props.theUser.lastName}
             </div>
           </NavLink>
         }
 
       </div>
     </nav>
-  )
+    </Breakpoint>
 
+    </div>
+  )
+}
 }
 
 export default Nav;

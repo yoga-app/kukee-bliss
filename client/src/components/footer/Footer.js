a10e1423ca455ad065aa28ecad136fb34a42e3ad
Artem Streltov <arstrel@gmail.com>
2021-02-21 13:07:19 -0500

    make it better

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
-import {Link} from 'react-router-dom';
-import "./footer.css";
-import Waiver from '../waiver/Waiver.js';
-import Liability from '../liability/Liability.js';
+import './footer.css';
+
+import React, { Component } from 'react';
+
 import Breakpoint from 'react-socks';
+import Liability from '../liability/Liability.js';
+import { Link } from 'react-router-dom';
+import Waiver from '../waiver/Waiver.js';
 
 class Footer extends Component {
   render() {
@@ -13,25 +15,51 @@ class Footer extends Component {
           <div className="footer-mobile">
             <div className="footer-middle">
               <ul>
-                <li><Link to='/faq'>FAQ</Link></li>
-                <li> <Waiver /></li>
-                <li><Liability /></li>
+                <li>
+                  <Link to="/faq">FAQ</Link>
+                </li>
+                <li>
+                  {' '}
+                  <Waiver />
+                </li>
+                <li>
+                  <Liability />
+                </li>
               </ul>
             </div>
             <div className="footer-right">
-
-              <a href="https://www.facebook.com/kukeeblissyoga/" target="_blank" rel="noopener noreferrer"
-              className="social-link">
-              <img src="/images/facebook.svg" alt="" className="social-icon"/>
+              <a
+                href="https://www.facebook.com/kukeeblissyoga/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="social-link"
+              >
+                <img
+                  src="/images/facebook.svg"
+                  alt=""
+                  className="social-icon"
+                />
               </a>
               <pre className="builtby-p-mobile">
-              Built by <a href="https://github.com/arstrel" target="_blank" rel="noopener noreferrer" className="builtby-link">
-              Artem  
-              </a> and <a href="https://github.com/taluama" target="_blank" rel="noopener noreferrer" className="builtby-link">
-              Talu 
-              </a>
+                Built by{' '}
+                <a
+                  href="https://github.com/arstrel"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="builtby-link"
+                >
+                  Artem
+                </a>{' '}
+                and{' '}
+                <a
+                  href="https://github.com/taluama"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="builtby-link"
+                >
+                  Talu
+                </a>
               </pre>
-
             </div>
           </div>
         </Breakpoint>
@@ -39,13 +67,16 @@ class Footer extends Component {
           <div className="footer">
             <div className="footer-left">
               {/* <p> Our Mission</p> */}
-              <p>Kukee Bliss Yoga was founded to put people first and to make yoga available to everyone. </p>
+              <p>
+                Kukee Bliss Yoga was founded to put people first and to make
+                yoga available to everyone.{' '}
+              </p>
             </div>
             <div className="footer-middle">
               <ul>
                 <li>
                   <span className="footer-hover">
-                    <Link to='/faq'>Frequently Asked Questions</Link>
+                    <Link to="/faq">Frequently Asked Questions</Link>
                   </span>
                 </li>
                 <li>
@@ -61,20 +92,33 @@ class Footer extends Component {
               </ul>
             </div>
             <div className="footer-right">
-              <a href="https://www.facebook.com/kukeeblissyoga/" target="_blank" rel="noopener noreferrer"
-              className="social-link">
-                <img src="/images/facebook.svg" alt="" className="social-icon"/>
-                <span className="footer-hover social-text">Follow us on Facebook</span>
+              <a
+                href="https://www.facebook.com/kukeeblissyoga/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="social-link"
+              >
+                <img
+                  src="/images/facebook.svg"
+                  alt=""
+                  className="social-icon"
+                />
+                <span className="footer-hover social-text">
+                  Follow us on Facebook
+                </span>
               </a>
 
               <pre className="builtby-p">
-                Built by <a href="https://github.com/arstrel" target="_blank" rel="noopener noreferrer" className="builtby-link">
+                Built by{' '}
+                <a
+                  href="https://github.com/arstrel"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="builtby-link"
+                >
                   Artem
-                </a> and <a href="https://github.com/taluama" target="_blank" rel="noopener noreferrer" className="builtby-link">
-                  Talu 
                 </a>
               </pre>
-
             </div>
           </div>
         </Breakpoint>
